54a3ae8de271fcfc042a0a4a2b1214fc2e6bcbf7
DemboJeremias <jeremiasdembo@gmail.com>
2018-04-06 17:53:28 +0100

    Create about Page

--- basket.php
+++ basket.php
@@ -1,38 +1,28 @@
 <?php require_once 'include\header.php';
 
-
 require_once 'database\DB_connection.php';
 require_once 'database\db_functions.php';
 require_once 'controller\submitBasket.php';
-
-
-
 ?>
 
     <table>
-        <thead>
-        <tr>
-            <th>Departure</th>
-            <th>Time</th>
-            <th>Destination</th>
-        </tr>
-        </thead>
-        <tbody>
+            <thead>
+                <tr>
+                <th>Departure</th>
+                <th>Time</th>
+                <th>Destination</th>
+                </tr>
+            </thead>
+
+            <tbody>
 
 
-        <?php foreach ($_SESSION['basket'] as $item): ?>
-            <tr>
-
+               <?php foreach ($_SESSION['basket'] as $item): ?>
+                 <tr>
                     <td><?=$item?></td>
-
-
-            </tr>
-
-
-        <?php endforeach;?>
-
-
-        </tbody>
+                 </tr>
+              <?php endforeach;?>
+            </tbody>
     </table>
 
 
--- include/header.php
+++ include/header.php
@@ -8,20 +8,26 @@
 
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="/flight/style/style.css" type="text/css">
+    <link rel="stylesheet" href="/flight/style/responsive.css" type="text/css">
+
     <link rel="stylesheet" href="style/style.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
 
     <script  src='flyGuys.js'></script>
 
 </head>
 <body>
-<ul class="nav">
-    <div class="logo"><a href="flyguysHome.html">FLY-GUYS</a></div>
-    <li><a href="home.php">Sign Up</a></li>
-    <li><a href="login.php">Log in</a></li>
-    <li><a href="about.php">About</a></li>
-    <li><a href="lala.php">Destinations</a></li>
-    <li><a href="basket.php">Basket</a></li>
 
-</ul>
+    <div class="col-lg-15 col-m12">
+        <ul class="nav">
+            <div class="logo"><a href="index.php">FLY-GUYS</a></div>
+        <!--    <li><a href="home.php">Sign Up</a></li>-->
+            <li><a href="login.php">Log in</a></li>
+            <li><a href="about.php">About</a></li>
+        <!--    <li><a href="dest.php">Destinations</a></li>-->
+            <li><a href="basket.php">Basket</a></li>
+
+        </ul>
+</div>
