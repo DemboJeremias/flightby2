52c2fa57721122be8ac6284a7a6aa3308200fef0
DemboJeremias <jeremiasdembo@gmail.com>
2018-04-09 21:37:37 +0100

    still forking

--- feedrss.php
+++ feedrss.php
@@ -3,7 +3,7 @@ $html = "";
 
 $url = "";
 $xml = simplexml_load_file($url);
-for($i = 0; $i < 10; $i++{
+for($i = 0; $i < 10; $i++){
     $title = $xml->feed->entry[$i]->title;
     $html .= "<P2>$title</P2>";
 }
